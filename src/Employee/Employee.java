4a9f9acae7cbf2f711a7d63b34b77116b279faa6
morozvitali <morozvitali99@gmail.com>
2021-12-17 02:12:23 +0200

    added class Test no extends setted

--- src/Employee/Employee.java
+++ src/Employee/Employee.java
@@ -2,11 +2,11 @@ package Employee;
 
 import java.util.Random;
 
-/*
-Создать класс Employee (int id, String name, double salary, double rate).
-Все поля приватные. Конструктор сделать со всеми параметрами.
-Сделать метод, который будет возвращать зп = ставка(salary) * rate.
- */
+
+//Создать класс Employee (int id, String name, double salary, double rate).
+//Все поля приватные. Конструктор сделать со всеми параметрами.
+//Сделать метод, который будет возвращать зп = ставка(salary) * rate.
+
 public class Employee {
     int id;
     String name;
@@ -29,10 +29,10 @@ public class Employee {
     }
 }
 
-/*
-Cоздать класс Developer, кот будет наследоваться от Employee. У девелопера помимо всего прочего есть кол-во строк кода (int codeLines).
-для девелопера зп высчитывается по другой формуле: зп = ставка(salary) * rate * codeLinew/random число от 100 до 1000.
-*/
+
+//Cоздать класс Developer, кот будет наследоваться от Employee. У девелопера помимо всего прочего есть кол-во строк кода (int codeLines).
+//для девелопера зп высчитывается по другой формуле: зп = ставка(salary) * rate * codeLines/random число от 100 до 1000.
+
 
 class Developer extends Employee {
     int id;
@@ -57,8 +57,51 @@ class Developer extends Employee {
     }
 }
 
-/*
-    Создать класс Manager, кот будет наследоваться от Employee. У менеджера помимо всего прочего есть кол-во проектов, на которых он работает (int projectsAmount).
-        Для менеджера зп высчитывается по стандартной формуле для сотрудника.
-        Создать класс Test, в котором создать менеджера и девелопера. вывести на экран инфо о них (toString) и вывесть зп итоговую для каждого.
-*/
+
+//    Создать класс Manager, кот будет наследоваться от Employee. У менеджера помимо всего прочего есть кол-во проектов, на которых он работает (int projectsAmount).
+//        Для менеджера зп высчитывается по стандартной формуле для сотрудника.
+
+
+class Manager extends Employee {
+    int id;
+    String name;
+    double salary;
+    double rate;
+    int projectsAmount;
+
+    Manager(int id, String name, double salary, double rate, int projectsAmount) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.rate = rate;
+        this.projectsAmount = projectsAmount;
+    }
+
+    double zpManagerCalc() {
+        return salary * rate;
+    }
+}
+
+//    Создать класс Test, в котором создать менеджера и девелопера. вывести на экран инфо о них (toString) и вывесть зп итоговую для каждого.
+
+class Test {
+    public static void main(String[] args) {
+        Manager bob = new Manager(11, "bob", 500, 4.4, 5);
+        Developer john = new Developer(32, "john", 1000, 8.8, 500);
+
+        System.out.println("id: " + bob.id + " | name: " + bob.name + " | salary: " + bob.salary + " | rate: " + bob.rate + " | project amounts: " + bob.projectsAmount);
+        System.out.println("id: " + john.id + " | name: " + john.name + " | salary: " + john.salary + " | rate: " + john.rate + " | code Lines: " + john.codeLines);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
